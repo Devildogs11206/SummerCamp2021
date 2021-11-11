975bb92e56ec04bdf8a02d1ffe830d60d1b49645
Devildogs11206 <devildogs11206@gmail.com>
2021-11-10 18:26:32 -0600

    added blue wearhouse and redwearhouse

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/BlueWarehouse.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/BlueWarehouse.java
@@ -2,14 +2,17 @@ package org.firstinspires.ftc.teamcode.opmodes;
 
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 
+import org.firstinspires.ftc.teamcode.internal.Robot;
+
 @Autonomous
 public class BlueWarehouse extends BlueOpMode {
     @Override
     protected void execute() {
         robot.drivePower = 0.5;
+        robot.lift(Robot.LiftMode.BACKWARD,500);
         robot.drive(1,0,0, 24);
         robot.turn(1,-45);
-        // TODO: Add code for delivering the pre-loaded freight
+        robot.intake(Robot.IntakeWheelMode.FORWARD,500);
         robot.drive(0,1,-90,30);
         robot.drive(-1,0,-90,36);
 
